1a523025c88ec3ec2cb9374217f2757a7f31c06b
N_DK <nguyenkhoadang135759@gmail.com>
2023-07-02 23:03:25 +0700

    change mongodb_url -> mongodb://127.0.0.1/ndk_sellWatch_dev

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,12 +1,12 @@
 
-
 const mongoose = require('mongoose');
 
 async function connect() {
     
     try {
         // show loading
-        await mongoose.connect(process.env.MONGODB_CONNECT_URI);
+        // process.env.MONGODB_CONNECT_URI
+        await mongoose.connect('mongodb://127.0.0.1/ndk_sellWatch_dev');
         console.log("Connect successfully");
         // hide loading
     } catch (error) {
